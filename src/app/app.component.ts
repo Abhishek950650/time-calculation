06c6340eea91bfe77bd4a6e6f99334b88700aed5
Abhishek Kumar <abhishek950650@gmail.com>
2024-04-24 23:52:41 +0530

    start code of time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'time-calculation';
+
+  ngOnInit(){
+  }
+
+  starttime(){
+    console.log('start');
+  }
+
+  endtime(){
+    console.log('end');
+  }
 }
