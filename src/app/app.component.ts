ff5ce15c1e4b15ee39462ddbd53202cb02f4d726
Abhishek Kumar <abhishek950650@gmail.com>
2024-04-25 21:31:37 +0530

    case 4 updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -82,4 +82,26 @@ export class AppComponent {
     console.log('total_hour =', total_hour);
 
   }
+
+  case4() {
+    /** you can use dynamic date and time according to your need
+     *  but i use static (date + time) to show an example */
+
+    // in that case day will change (PM to AM)
+    var start_date_time = "2000-01-01T" + "22:45"; // here you can use current date
+    var end_date_time = "2000-01-02T" + "06:10"; // here you can use next date
+
+    var start_epoch_seconds = ((new Date(start_date_time)).getTime()) / 1000;
+    var end_epoch_seconds = ((new Date(end_date_time)).getTime()) / 1000;
+
+    var total_epoch_seconds = (end_epoch_seconds - start_epoch_seconds);
+
+    var total_minutes = total_epoch_seconds / 60; // here total time difference come in to minute;
+
+    var total_hour = total_minutes / 60; // here total time difference come in to hour;
+
+    console.log('total_minutes =', total_minutes);
+    console.log('total_hour =', total_hour);
+
+  }
 }
