058af1201d76be6756caee190305cea6510cc9c7
Abhishek Kumar <abhishek950650@gmail.com>
2024-04-25 21:00:38 +0530

    add first case of time calculation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,14 +8,36 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'time-calculation';
 
-  ngOnInit(){
+  ngOnInit() {
+    this.case1(); // case AM to AM
   }
 
-  starttime(){
+  starttime() {
     console.log('start');
   }
 
-  endtime(){
+  endtime() {
     console.log('end');
+  }
+
+  case1() {
+    /** you can use dynamic date and time according to your need
+     *  but i use static (date + time) to show an example */
+
+    var start_date_time = "2000-01-01T" + "01:30"; // here you can use current date
+    var end_date_time = "2000-01-01T" + "08:15"; // here you can use current date
+
+    var start_epoch_seconds = ((new Date(start_date_time)).getTime()) / 1000;
+    var end_epoch_seconds = ((new Date(end_date_time)).getTime()) / 1000;
+
+    var total_epoch_seconds = (end_epoch_seconds - start_epoch_seconds);
+
+    var total_minutes = total_epoch_seconds / 60; // here total time difference come in to minute;
+
+    var total_hour = total_minutes / 60; // here total time difference come in to hour;
+
+    console.log('total_minutes =', total_minutes);
+    console.log('total_hour =', total_hour);
+
   }
 }
